8fb73a22ecc40455dc05cc76e6528ab3d6194aae
david cochran <about.trout@gmail.com>
2024-02-11 19:21:52 -0800

    rename to boatfinder

--- boatfinder.py
+++ boatfinder.py
@@ -11,11 +11,15 @@ from transformers import DetrImageProcessor, DetrForObjectDetection
 
 class BoatFinder:
     def __init__(self):
-        logging.info(f"BoatFinder initializing ...") 
+        logging.info(f"BoatFinder initializing ...")
         t0 = perf_counter()
         # https://huggingface.co/facebook/detr-resnet-50
-        self.processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-        self.model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+        self.processor = DetrImageProcessor.from_pretrained(
+            "facebook/detr-resnet-50", revision="no_timm"
+        )
+        self.model = DetrForObjectDetection.from_pretrained(
+            "facebook/detr-resnet-50", revision="no_timm"
+        )
         logging.info(f"BoatFinder initialized! took {perf_counter() - t0} seconds")
 
     def find(self, img):
@@ -31,7 +35,9 @@ class BoatFinder:
         inputs = self.processor(images=image, return_tensors="pt")
         outputs = self.model(**inputs)
         target_sizes = torch.tensor([image.size[::-1]])
-        return self.processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.5)[0]
+        return self.processor.post_process_object_detection(
+            outputs, target_sizes=target_sizes, threshold=0.5
+        )[0]
 
 
 if __name__ == "__main__":
@@ -39,7 +45,7 @@ if __name__ == "__main__":
         print(f"USAGE: {sys.argv[0]} /path/to/file_that_might_have_boats.jpg")
         sys.exit(1)
 
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
     bf = BoatFinder()
     for img_file in sys.argv[1:]:
