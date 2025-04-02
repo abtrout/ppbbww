abcf58eaa84bb49c4a8be269e7435a62606a9888
david cochran <about.trout@gmail.com>
2025-04-01 20:06:34 -0700

    remove gallery
    i stopped updating it

--- README.md
+++ README.md
@@ -2,10 +2,6 @@ _Pillar Point boats, birds, [~~and waves~~](https://github.com/abtrout/ppbbww/is
 
 [`ppbw`](/ppboatwatch/ppbw.py) is a Python project that samples frames from the [Mavericks](https://en.wikipedia.org/wiki/Mavericks,_California) Surfline cam and runs [facebook/detr-resnet-50](https://huggingface.co/facebook/detr-resnet-50) on them to detect various objects, e.g. boats and birds. Matches are archived in a SQLite database and announced to a shared Slack channel to be enjoyed with friends.
 
-After running this for a while, _many neat images_ were captured, which lead to the creation of the gallery. There's a small [`curator`](/ppboatwatch/gallery.py#L77-L80) web app for hand picking gallery-worthy images from the matches database, and a script to [`generate`](/ppboatwatch/gallery.py#L83-L114) a static Jekyll site that's suitable to be deployed on my [`gh-pages`](https://github.com/abtrout/ppbbww/tree/gh-pages) branch. I curate by hand and manually push updates sometimes, ¯\\\_(ツ)_/¯.
-
-__[Check out the gallery here!](https://abtrout.github.io/ppbbww)__
-
 #### Getting started
 
 ```
--- ppboatwatch/archive.py
+++ ppboatwatch/archive.py
@@ -10,8 +10,7 @@ CREATE TABLE IF NOT EXISTS matches (
     filename,
     label,
     score,
-    x0, y0, x1, y1,
-    gallery DEFAULT NULL
+    x0, y0, x1, y1
 )"""
 
 ADD_MATCH_SQL = """
@@ -19,23 +18,8 @@ INSERT INTO matches (ts, filename, label, score, x0, y0, x1, y1)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
 
 LIST_MATCHES_SQL = """
-SELECT ts, filename, label, score, x0, y0, x1, y1, gallery
+SELECT ts, filename, label, score, x0, y0, x1, y1
 FROM matches ORDER BY ts DESC"""
-
-LIST_GALLERY_MATCHES_SQL = """
-SELECT ts, filename, label, score, x0, y0, x1, y1, gallery
-FROM matches WHERE gallery IS NOT NULL ORDER BY ts DESC"""
-
-UPDATE_GALLERY_SQL = """
-UPDATE matches SET gallery = ? WHERE ts = ?"""
-
-GET_MATCH_PAGE_SQL = """
-SELECT ts, filename, label, score, x0, y0, x1, y1, gallery, previous_ts, next_ts
-FROM (SELECT *,
-    LAG(ts, 1) OVER (ORDER BY ts DESC) AS next_ts,
-    LEAD(ts, 1) OVER (ORDER BY ts DESC) AS previous_ts
-    FROM matches ORDER BY ts DESC
-) WHERE ts = ?"""
 
 
 class Archive:
@@ -55,22 +39,3 @@ class Archive:
         with closing(self.con.cursor()) as cur:
             return cur.execute(LIST_MATCHES_SQL).fetchall()
 
-    def list_gallery_matches(self):
-        with closing(self.con.cursor()) as cur:
-            return cur.execute(LIST_GALLERY_MATCHES_SQL).fetchall()
-
-    def get_match_page(self, ts):
-        with closing(self.con.cursor()) as cur:
-            return cur.execute(GET_MATCH_PAGE_SQL, (ts,)).fetchone()
-
-    def set_gallery(self, ts):
-        return self.__update_gallery(ts, 1)
-
-    def unset_gallery(self, ts):
-        return self.__update_gallery(ts, None)
-
-    def __update_gallery(self, ts, value):
-        with closing(self.con.cursor()) as cur:
-            res = cur.execute(UPDATE_GALLERY_SQL, (value, ts))
-            self.con.commit()
-            return res
